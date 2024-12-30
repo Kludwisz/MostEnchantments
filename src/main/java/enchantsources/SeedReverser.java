681f3e0948037e006339574572601d8c5257b2a6
Kludwisz <kludwisz@gmail.com>
2024-12-30 16:59:15 +0100

    Document SeedReverser.java

--- src/main/java/enchantsources/SeedReverser.java
+++ src/main/java/enchantsources/SeedReverser.java
@@ -4,6 +4,7 @@ import com.seedfinding.mccore.util.data.Pair;
 import com.seedfinding.mccore.util.math.NextLongReverser;
 import com.seedfinding.mccore.util.pos.CPos;
 import com.seedfinding.mccore.version.MCVersion;
+import com.seedfinding.mccore.version.UnsupportedVersion;
 import com.seedfinding.mcmath.util.Mth;
 import com.seedfinding.mcreversal.ChunkRandomReverser;
 import com.seedfinding.mcseed.lcg.LCG;
@@ -18,21 +19,44 @@ public class SeedReverser {
     private int featureSalt = 0;
     private MCVersion version = MCVersion.v1_16_1;
 
+    /**
+     * Creates a chunk-area-bounded SeedReverser.
+     * @param chunkMin the area's minimum chunk position, inclusive
+     * @param chunkMax the area's maximum chunk position, inclusive
+     */
     public SeedReverser(CPos chunkMin, CPos chunkMax) {
         this.chunkMin = chunkMin;
         this.chunkMax = chunkMax;
     }
 
+    /**
+     * Specifies the feature salt (also known as the decoration salt) to be used in the reversal process.
+     */
     public SeedReverser withFeatureSalt(int featureSalt) {
         this.featureSalt = featureSalt;
         return this;
     }
 
+    /**
+     * Specifies the version to be used in the reversal process.
+     */
     public SeedReverser forVersion(MCVersion version) {
+        if (version == null)
+            return this;
+        if (version.isNewerOrEqualTo(MCVersion.v1_18) || version.isOlderThan(MCVersion.v1_13))
+            throw new UnsupportedVersion(version, "SeedReverser's methods");
+
         this.version = version;
         return this;
     }
 
+    /**
+     * Reverses a loot seed to a list of world seeds and chunk positions where the loot seed will be
+     * generated for the first chest in the given chunk, assuming no previous random calls happened.
+     * The resulting chunks are always within the boundaries specified in the constructor.
+     * @param lootSeed the loot seed to reverse
+     * @return a list of pairs, where the first element is the world seed and the second element is the chunk position
+     */
     public List<Pair<Long, CPos>> reverseLootSeed(long lootSeed) {
         List<Long> internalSeeds = NextLongReverser.getSeeds(lootSeed & Mth.MASK_48);
         if (internalSeeds.isEmpty())
@@ -43,6 +67,12 @@ public class SeedReverser {
                 .flatMap(List::stream).toList();
     }
 
+    /**
+     * Reverses a decorator seed to a list of world seeds and positions of chunks that have the given decorator,
+     * within the boundaries specified in the constructor.
+     * @param decoratorSeed the decorator seed
+     * @return a list of pairs, where the first element is the world seed and the second element is the chunk position
+     */
     public List<Pair<Long, CPos>> reverseDecoratorSeed(long decoratorSeed) {
         long popseed = ChunkRandomReverser.reverseDecoratorSeed(decoratorSeed, featureSalt % 10000, featureSalt / 10000, version);
 
@@ -62,6 +92,9 @@ public class SeedReverser {
         return results;
     }
 
+    /**
+     * Returns whether it is possible or not for a chest with the given loot seed to generate.
+     */
     public static boolean canGetLootSeed(long lootSeed) {
         return !NextLongReverser.getSeeds(lootSeed & Mth.MASK_48).isEmpty();
     }
